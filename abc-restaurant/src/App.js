4f62906e74411e9e0bc45a83a23ef88efc14b2e3
ShenuKavi <shenukavindya918@gmail.com>
2024-09-11 05:15:16 +0530

    Update App.js

--- abc-restaurant/src/App.js
+++ abc-restaurant/src/App.js
@@ -4,8 +4,8 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import OnlineOrder from "./pages/OnlineOrder";
-// import AdminMenu from "./pages/AdminMenu";
-// import AdminReport from "./pages/AdminReport";
+// import AdminMenu from "./pages/AdminMenu"; // Uncomment when needed
+// import AdminReport from "./pages/AdminReport"; // Uncomment when needed
 import Payment from "./pages/Payment";
 import FeedBack from "./pages/FeedBack";
 import Admin from "./pages/admin/Admin";
@@ -13,6 +13,7 @@ import UpdateUser from "./pages/admin/UpdateUser";
 import MenuItem from "./pages/admin/MenuItem";
 import Staff from "./pages/staff/staff";
 import Reservation from "./pages/Reservation";
+
 function App() {
   return (
     <Router>
@@ -23,13 +24,13 @@ function App() {
           <Route path="/signup" element={<Register />} />
           <Route path="/onlineorder" element={<OnlineOrder />} />
           <Route path="/payment" element={<Payment />} />
-          <Route path="/Feedback" element={<FeedBack />} />
-          <Route path="/admin" element={<Admin />}/>
+          <Route path="/feedback" element={<FeedBack />} /> {/* Make route lowercase */}
+          <Route path="/admin" element={<Admin />} />
           <Route path="/admin/update-user/:id" element={<UpdateUser />} />
           <Route path="/admin/add-item" element={<MenuItem />} />
           <Route path="/admin/menu-item/:id" element={<MenuItem />} />
           <Route path="/staff" element={<Staff />} />
-          <Route path="/reservation" element={<Reservation   />}/>
+          <Route path="/reservation" element={<Reservation />} />
         </Routes>
       </div>
     </Router>
