29b8bddb1e1299066fed515a1c3e4ecd7cb24139
ShenuKavi <shenukavindya918@gmail.com>
2024-09-11 11:58:17 +0530

    Update Contact.js

--- abc-restaurant/src/pages/Contact.js
+++ abc-restaurant/src/pages/Contact.js
@@ -9,10 +9,22 @@ function Contact() {
       </div>
       <div className="d-flex">
         <div className="w-50 text-start ps-5">
-          <div className="mb-1">ABC Restaurant</div>
-          <div className="mb-1">No. 10, Flower Road, Colombo</div>
+          <div className="mb-1">ABC Restaurant Colombo</div>
+          <div className="mb-1">No. 107/A, Baseline Road, Colombo 9</div>
+          <div className="mb-1">Tel: 0112-920-500</div>
+          <div className="mb-1">Email: abcrestaurantcmb@abc.lk</div>
+        </div>
+        <div className="w-50 text-start ps-5">
+          <div className="mb-1">ABC Restaurant Galle</div>
+          <div className="mb-1">No. 264/H, TS Road, Galle</div>
           <div className="mb-1">Tel: 0112-236-569</div>
-          <div className="mb-1">Email: abcrestaurant@abc.lk</div>
+          <div className="mb-1">Email: abcrestaurantgall@abc.lk</div>
+        </div>
+        <div className="w-50 text-start ps-5">
+          <div className="mb-1">ABC Restaurant Kandy</div>
+          <div className="mb-1">No. 10, Queens Road, Kandy</div>
+          <div className="mb-1">Tel: 0112-236-569</div>
+          <div className="mb-1">Email: abcrestaurantknd@abc.lk</div>
         </div>
         <div className="w-50 d-flex justify-content-center align-items-end">
           <div>&copy; All Right Reserved 2024 ABC Restaurant</div>
