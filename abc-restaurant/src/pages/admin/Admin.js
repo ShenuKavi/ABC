e663e63ea13cfe05ba423f5d7474aef01e93ad2f
ShenuKavi <shenukavindya918@gmail.com>
2024-09-11 10:45:45 +0530

    Update Admin.js

--- abc-restaurant/src/pages/admin/Admin.js
+++ abc-restaurant/src/pages/admin/Admin.js
@@ -69,11 +69,20 @@ function Admin() {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: `url("/assets/Adminbg.jpg")`, // Your image path
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh", // Ensure it covers the whole page
+        color: "white", // Text color for better contrast with the background
+      }}
+    >
       <div id="admin-header">
         <div className="px-5 py-2 d-flex justify-content-between bg-dark bg-opacity-25">
           <div>
-            <img src="assets/Loogo1.png" alt="website-logo" className="w-20" />
+            <img src="assets/Logoo.png" alt="website-logo" className="w-20" />
           </div>
           <div className="d-flex align-items-center">
             <div className="me-3">{user}</div>
@@ -86,6 +95,7 @@ function Admin() {
         </div>
       </div>
 
+      {/* User Management */}
       <div id="user-management">
         <div className="user-mng pb-4 pt-3 bg-dark bg-opacity-10">
           <h1>Users</h1>
@@ -130,122 +140,66 @@ function Admin() {
         </div>
       </div>
 
+      {/* Reservation Management */}
       <div className="services-mng w-100 d-flex">
         <div
           id="reservation-management"
           className="w-50 py-4 bg-dark bg-opacity-75 text-light"
         >
-          <div>
-            <h1 className="mb-3">Reservation</h1>
-            <div className="h-100">
-              <table className="w-75 mt-2 mx-auto table table-light table-striped text-dark">
-                <thead>
-                  <tr className="rounded-top">
-                    <th>ID</th>
-                    <th>User Email</th>
-                    <th>Branch</th>
-                    <th>Time</th>
-                    <th>Pax</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {reservations.map((reservation, index) => (
-                    <tr key={index}>
-                      <td>{reservation.id}</td>
-                      <td>{reservation.user_id}</td>
-                      <td>{reservation.branch}</td>
-                      <td>{reservation.time}</td>
-                      <td>{reservation.pax}</td>
-                      <td>
-                        <button className="me-2 btn btn-success">Edit</button>
-                        <button className="btn btn-danger">Delete</button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
+          <h1 className="mb-3">Reservation</h1>
+          <table className="w-75 mt-2 mx-auto table table-light table-striped text-dark">
+            <thead>
+              <tr className="rounded-top">
+                <th>ID</th>
+                <th>User Email</th>
+                <th>Branch</th>
+                <th>Time</th>
+                <th>Pax</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservations.map((reservation, index) => (
+                <tr key={index}>
+                  <td>{reservation.id}</td>
+                  <td>{reservation.user_id}</td>
+                  <td>{reservation.branch}</td>
+                  <td>{reservation.time}</td>
+                  <td>{reservation.pax}</td>
+                  <td>
+                    <button className="me-2 btn btn-success">Edit</button>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
+
+        {/* Order Management */}
         <div id="order-management" className="w-50">
           <div className="py-4 bg-dark text-light">
             <h1 className="mb-3">Orders</h1>
-            <div className="h-100">
-              <table className="w-75 mt-2 mx-auto table table-light table-striped text-dark">
-                <thead>
-                  <tr className="rounded-top">
-                    <th>ID</th>
-                    <th>User Email</th>
-                    <th>Items</th>
-                    <th>Total Price</th>
-                    <th>Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((order, index) => (
-                    <tr key={index}>
-                      <td>{order.id}</td>
-                      <td>{order.user_id}</td>
-                      <td>{order.menu_items.length}</td>
-                      <td>Rs. {order.total}</td>
-                      <td>
-                        <button className="me-2 btn btn-success">Edit</button>
-                        <button className="btn btn-danger">Delete</button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div id="menu-management">
-        <div className="py-4">
-          <h1 className="mb-3">Menus</h1>
-          <div className="h-100">
-            <table className="w-75 mt-2 mx-auto table table-dark table-striped">
+            <table className="w-75 mt-2 mx-auto table table-light table-striped text-dark">
               <thead>
-                <tr>
+                <tr className="rounded-top">
                   <th>ID</th>
-                  <th>Item Name</th>
-                  <th>Unit Price</th>
-                  <th>Category</th>
-                  <th>Photo</th>
+                  <th>User Email</th>
+                  <th>Items</th>
+                  <th>Total Price</th>
                   <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
-                {menus.map((menu, index) => (
+                {orders.map((order, index) => (
                   <tr key={index}>
-                    <td>{menu.id}</td>
-                    <td>{menu.item_name}</td>
-                    <td>Rs. {menu.price}</td>
-                    <td>{menu.category}</td>
-                    <td className="d-flex justify-content-center align-items-bottom">
-                      <div style={{ maxWidth: "100px" }}>
-                        <img
-                          src={menu.photo_url}
-                          alt="item-photo"
-                          className="w-100"
-                          style={{ objectFit: "cover", height: "80px" }}
-                        />
-                      </div>
-                    </td>
+                    <td>{order.id}</td>
+                    <td>{order.user_id}</td>
+                    <td>{order.menu_items.length}</td>
+                    <td>Rs. {order.total}</td>
                     <td>
-                      <Link to={`/admin/menu-item/${menu.id}`}>
-                        <button className="me-2 w-20 btn btn-success">
-                          Edit
-                        </button>
-                      </Link>
-                      <button
-                        onClick={() => handleDeleteMenuItem(menu.id)}
-                        className="w-20 btn btn-danger"
-                      >
-                        Delete
-                      </button>
+                      <button className="me-2 btn btn-success">Edit</button>
+                      <button className="btn btn-danger">Delete</button>
                     </td>
                   </tr>
                 ))}
@@ -254,6 +208,60 @@ function Admin() {
           </div>
         </div>
       </div>
+
+      {/* Menu Management */}
+      <div id="menu-management">
+        <div className="py-4">
+          <h1 className="mb-3">Menus</h1>
+          <table className="w-75 mt-2 mx-auto table table-dark table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Item Name</th>
+                <th>Unit Price</th>
+                <th>Category</th>
+                <th>Photo</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menus.map((menu, index) => (
+                <tr key={index}>
+                  <td>{menu.id}</td>
+                  <td>{menu.item_name}</td>
+                  <td>Rs. {menu.price}</td>
+                  <td>{menu.category}</td>
+                  <td className="d-flex justify-content-center align-items-bottom">
+                    <div style={{ maxWidth: "100px" }}>
+                      <img
+                        src={menu.photo_url}
+                        alt="item-photo"
+                        className="w-100"
+                        style={{ objectFit: "cover", height: "80px" }}
+                      />
+                    </div>
+                  </td>
+                  <td>
+                    <Link to={`/admin/menu-item/${menu.id}`}>
+                      <button className="me-2 w-20 btn btn-success">
+                        Edit
+                      </button>
+                    </Link>
+                    <button
+                      onClick={() => handleDeleteMenuItem(menu.id)}
+                      className="w-20 btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      {/* Contact */}
       <div className="bg-dark">
         <div className="w-75 mx-auto">
           <Contact />
